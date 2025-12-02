a904106f4bf4a26bffa831d7a045c14cea0a0b66
theothegoth <huseyinguven24@hotmail.com>
2025-12-03 00:06:35 +0300

    Update navigation logo to logo2192

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -34,7 +34,7 @@ const Navigation = () => {
               className="flex items-center space-x-2 text-2xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent hover:from-blue-700 hover:to-purple-700 transition-all"
             >
               <img
-                src="/logo192.png"
+                src="/logo2192.png"
                 alt="Gaste Logo"
                 className="w-8 h-8 object-contain"
               />
